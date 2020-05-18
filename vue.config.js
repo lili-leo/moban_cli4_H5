208ae953644f14d5dd391e74d09deea1b94f8561
haden <haden123>
2020-05-18 16:39:39 +0800

    关闭eslint 和prettier的检查

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const productionGzipExtensions = ['js', 'html', 'css'];
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const IS_PROD = process.env.NODE_ENV === 'production';
 module.exports = {
-  // extends: ["plugin:vue/essential"], // "@vue/prettier"关闭检查
+  extends: ["plugin:vue/essential"], // "@vue/prettier"关闭检查
   configureWebpack: {
     resolve: {
       alias: {
@@ -13,6 +13,19 @@ module.exports = {
       }
     }
   },
+  parserOptions: {
+    parser: 'babel-eslint'
+},
+  rules: {
+    //严格的检查缩进问题，不是报错，我们可以关闭这个检查规则,然后在终端输入npm run dev
+    "indent": ["off", 2],
+    //使用eslint时，严格模式下，报错Missing space before function parentheses的问题，意思是在方法名和刮号之间需要有一格空格。
+    'space-before-function-paren': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    //关闭prettier
+    "prettier/prettier": "off"
+},
     //打包优化moment时间函数
     // plugins: [
     //     new webpack.ContextReplacementPlugin(
